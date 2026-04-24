b0491a9f16ffd0f54eda98edc75c0c87f039a8be
marrthecreatorr <marlenoemi14@gmail.com>
2026-04-24 17:05:02 +0000

    fav

--- hello.c
+++ hello.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 int main(void){
-     char name [15];
+     char YourName[15];
     printf("What's your name?: ");
-    printf("%s", &yourname);
-    printf("your name is: %s \n", yourname);
-    printf("hellou %s)
+    scanf("%s", &YourName);
+    printf("Your name is: %s \n", YourName);
+    printf("Hello %s) \n", YourName);
 
-
+    char YourFavoriteColor[50];
+    printf("Your favorite color is: ");
+    scanf("%s", &YourFavoriteColor);
+    printf("Your favorite color is: %s \n", YourFavoriteColor);
 }
