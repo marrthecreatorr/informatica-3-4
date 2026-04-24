f5699c5677507c3e2bfb7e11248511bb834c9480
marrthecreatorr <marlenoemi14@gmail.com>
2026-04-24 14:59:08 +0000

    fav

--- hello.c
+++ hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+int main(void){
+     char name [15];
+    printf("What's your name?: ");
+    printf("%s", &yourname);
+    printf("your name is: %s \n", yourname);
+    printf("hellou %s)
 
-int main(void)
-{
-    printf("Hello Marlen! \n");
+
 }
