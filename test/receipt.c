08e473bf185860f6324cc51b0f6bac781204a646
marrthecreatorr <marlenoemi14@gmail.com>
2026-04-29 17:28:14 +0000

    receipt

---
+++ test/receipt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (void)
+char item ('4')
+    printf("item name")
+    scanf("%d", &item name);
+
+    int ("Price:");
+    printf("Price: \n ")
+    scanf("%2.f" , &Price);
+
+    int quantity1;
+    printf("Quantity1: ");
+    scanf("%d", &Quiantity1);
+
+    int total;
+
+
+}
