8d35960b22f467c53437d981c730b3a6145bd64f
marrthecreatorr <marlenoemi14@gmail.com>
2026-05-11 16:55:59 +0000

    temp3

--- temp3.c
+++ temp3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-//Prototype
-void c_to_f(void)
-void f_to_c(void)
+//1. Prototype
+void c_to_f(void);
+void f_to_c(void);
 
 int main(void) {
     int user_response;
@@ -11,7 +11,7 @@ int main(void) {
     printf("2 Fahrenheit to Celcius\n");
     scanf("%d", &user_response);
 
-    if (user_responce == 1){
+    if (user_response == 1){
         c_to_f();
     }else if(user_response == 2){
         f_to_c();
@@ -26,6 +26,14 @@ void c_to_f(void){
         int c;
         printf("What is the temperature in celcius?: ");
         scanf("%d" ,&c);
-        int c = (c *1.8) + 32;
+        int f = (c *1.8) + 32;
         printf("%d°C = %d°F\n", c, f);
+}
+
+void f_to_c(void){
+    int f;
+    printf("What is the temperature in Fahrenheit?: ");
+    scanf ("%d",&f);
+    int c = (f - 32) / 1.8;
+    printf("%d°F = %d°C\n", f, c);
 }
